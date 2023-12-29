ebafed35d12aadce3ab3ba738866a5df7580ea67
ангелина <linaprusak@gmail.com>
2023-12-30 02:01:08 +0300

    Превый уровень и декор

---
+++ levels/мебель.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="мебель" tilewidth="62" tileheight="46" tilecount="22" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <transformations hflip="0" vflip="0" rotate="1" preferuntransformed="0"/>
+ <tile id="18">
+  <image width="32" height="32" source="кварц норм.jpg"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="стены.jpg"/>
+ </tile>
+ <tile id="0">
+  <image width="17" height="42" source="огнетушитель.png"/>
+ </tile>
+ <tile id="1">
+  <image width="7" height="18" source="цветок 3.png"/>
+ </tile>
+ <tile id="2">
+  <image width="11" height="15" source="цветок 2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="12" height="16" source="цветок 1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="43" height="26" source="доска с картинками.png"/>
+ </tile>
+ <tile id="5">
+  <image width="43" height="26" source="доска с графиками.png"/>
+ </tile>
+ <tile id="6">
+  <image width="43" height="26" source="доска money.png"/>
+ </tile>
+ <tile id="7">
+  <image width="25" height="26" source="часы.png"/>
+ </tile>
+ <tile id="8">
+  <image width="14" height="35" source="1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="26" height="23" source="пуфик 1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="18" height="25" source="стул.png"/>
+ </tile>
+ <tile id="11">
+  <image width="11" height="23" source="корзина.png"/>
+ </tile>
+ <tile id="12">
+  <image width="20" height="21" source="книга 3.png"/>
+ </tile>
+ <tile id="13">
+  <image width="18" height="16" source="книга 2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="25" height="46" source="книги валяются.png"/>
+ </tile>
+ <tile id="15">
+  <image width="42" height="38" source="шкаф 1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="62" height="40" source="шкаф.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="кварц норм.jpg"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="окно.jpg"/>
+ </tile>
+ <tile id="29">
+  <image width="52" height="26" source="выход (1).png"/>
+ </tile>
+ <wangsets>
+  <wangset name="Безымянный набор" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>

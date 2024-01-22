7e4b53856c86b0a4e3a3f8776ebe9a31d501221b
ангелина <linaprusak@gmail.com>
2024-01-22 09:29:28 +0300

    комментарии

--- files/hero.py
+++ files/hero.py
@@ -25,43 +25,42 @@ class Border(pygame.sprite.Sprite):
 class Hero(pygame.sprite.Sprite):
     def __init__(self, x, y, person, font):
         pygame.sprite.Sprite.__init__(self)
-        self.name = Text(person, font, x + 10, y - 10, (20, 10, 30))
-        self.person = person
-        self.xvel = 0
-        self.w, self.h = 19, 40
-        self.coords_list = []
+        self.name = Text(person, font, x + 10, y - 10, (20, 10, 30))  # рисовка имени персонажа над головой
+        self.person = person   # имя персонажа
+        self.w, self.h = 19, 40  # ширина и высота персонажа
+        self.coords_list = []  # для получения последних координат, на которых был игрок, для уровня со спасением
+        # другого персонажа
         self.image = pygame.Surface((self.w, self.h))
         self.image.fill(pygame.Color(COLOR))
         self.rect = pygame.Rect(x, y, self.w, self.h)
         self.yvel = 0  # скорость вертикального перемещения
-        self.onGround = False
+        self.xvel = 0  # скорость горизонтального перемещения
+        self.onGround = False  # может ли падать игрок
         self.image.set_colorkey(pygame.Color(COLOR))  # делаем фон прозрачным
         self.delay, self.right, self.left, self.jump_right, self.jump_left, self.stay = get_animation(person)
+        # для получения анимации персонажа
         self.stay.blit(self.image, (0, 0))  # По умолчанию, стоим
 
-    def draw_name(self, screen, camera):
+    def draw_name(self, screen, camera):  # рисуется имя над головой
         screen.blit(self.name, camera.apply(self.name))
-
-    def update(self):
-        pass
 
     def draw(self, screen, camera):  # Выводим себя на экран
         screen.blit(self.image, camera.apply(self))
         # self.draw_name(screen, camera)
 
     def move(self, left, right, up, platforms, character):
-        if left:
-            self.xvel = -5  # Лево
+        if left:  # если нажата клавиша для движения влево
+            self.xvel = -5  # идем влево
             self.image.fill(pygame.Color(COLOR))
             if up:  # для прыжка влево есть отдельная анимация
                 self.jump_left.blit(self.image, (0, 0))
             else:
                 self.left.blit(self.image, (0, 0))
 
-        if right:
-            self.xvel = 5  # Право
+        if right:  # если нажата клавиша для движения вправо
+            self.xvel = 5  # идем вправо
             self.image.fill(pygame.Color(COLOR))
-            if up:
+            if up:  # отдельная анимация для прыжка
                 self.jump_right.blit(self.image, (0, 0))
             else:
                 self.right.blit(self.image, (0, 0))
@@ -71,23 +70,23 @@ class Hero(pygame.sprite.Sprite):
             self.image.fill(pygame.Color(COLOR))
             self.stay.blit(self.image, (0, 0))
 
-        if up:
-            if self.onGround:
-                self.yvel = -JUMP_POWER * up
-        if not self.onGround:
-            self.yvel += GRAVITY
-        self.rect.y += self.yvel
+        if up:  # если игрок прыгает
+            if self.onGround:  # и находится на земле
+                self.yvel = -JUMP_POWER * up  # прыгаем в зависимости от прыжка(простой, двойной)
+        if not self.onGround:  # если игрок не на земле
+            self.yvel += GRAVITY  # опускаем его
+        self.rect.y += self.yvel  # переносим положение игрока
         self.collide(0, self.yvel, platforms, character)
 
-        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.rect.x += self.xvel  # переносим положение игрока
         self.collide(self.xvel, 0, platforms, character)
 
-        if isinstance(character, Students):
-            if character.flag:
+        if isinstance(character, Students):  # если на уровне есть доп персонаж
+            if character.flag:  # и до него уже добежал игрок
                 character.image.fill(pygame.Color(COLOR))
-                if self.get_position()[:2] not in self.coords_list:
+                if self.get_position()[:2] not in self.coords_list:  # меняет положение на последние координаты игрока
                     self.coords_list.append(self.get_position()[:2])
-                if left:
+                if left:  # делаем анимацию в зависимости от направления движения
                     if up:
                         character.jump_left.blit(character.image, (0, 0))
                     else:
@@ -135,5 +134,5 @@ class Hero(pygame.sprite.Sprite):
         if pygame.sprite.collide_rect(self, a):  # запрет на выход из карты через вход
             self.rect.left = a.rect.right
 
-    def get_position(self):
+    def get_position(self):  #получение координат персонажа
         return self.rect.x, self.rect.y, self.rect.right - self.rect.left,  self.rect.bottom - self.rect.top
--- files/people.py
+++ files/people.py
@@ -6,52 +6,54 @@ from files.functions import Border
 COLOR = '#090909'
 
 
-class Teacher(pygame.sprite.Sprite):
+class Teacher(pygame.sprite.Sprite):  # класс учителя для последнего уровня
     def __init__(self, x, y):
         super().__init__()
         self.person = 'Иван Дмитриевич'
         _, self.right, self.left, _, _, self.stay = get_animation('Иван Дмитриевич', (21, 43))
-        self.x, self.y = x, y
+        # получение анимации
         w, h = 21, 43
-        self.speed = 3
+        self.speed = 4  # скорость передвижения
         self.image = pygame.Surface((w, h))
         self.image.fill(pygame.Color(COLOR))
         self.rect = pygame.Rect(x, y, w, h)
         self.image.set_colorkey((9, 9, 9))
-        self.stay.blit(self.image, (0, 0))
+        self.stay.blit(self.image, (0, 0))  # изначально стоит
 
-    def get_position(self):
-        return self.x, self.y, 19, 40
+    def get_position(self):  # получение координат
+        return self.rect.x, self.rect.y, 19, 40
 
     def move(self, labirint):
-        self.image.fill(pygame.Color(COLOR))
-        if self.speed > 0:
-            self.right.blit(self.image, (0, 0))
+        self.image.fill(pygame.Color(COLOR))   # обновляем анимацию
+        if self.speed > 0:  # если бежит вправо
+            self.right.blit(self.image, (0, 0))  # ставим анимацию движения вправо
             if (labirint.get_tile_id((self.rect.x + 32, self.rect.y - 16)) not in labirint.free_tiles or
                     labirint.get_tile_id((self.rect.x, self.rect.y + 50)) in labirint.free_tiles):
+                # если учитель упирается в стенку или платформа заканчивается, то разворачивается
                 self.speed *= -1
-        elif self.speed < 0:
+        elif self.speed < 0:  # если бежит влево
             self.left.blit(self.image, (0, 0))
             if (labirint.get_tile_id((self.rect.x, self.rect.y - 16)) not in labirint.free_tiles or
                     labirint.get_tile_id((self.rect.x, self.rect.y + 50)) in labirint.free_tiles):
+                # если учитель упирается в стенку или платформа заканчивается, то разворачивается
                 self.speed *= -1
         self.rect.x += self.speed
 
 
-class Students(pygame.sprite.Sprite):
+class Students(pygame.sprite.Sprite):  # класс доп персонажа, которого игрок спасает
     def __init__(self, x, y, person):
         super().__init__()
-        self.person = person
-        self.difference = None
-        self.x, self.y = x, y
-        w, h = 19, 40
-        self.image = pygame.Surface((w, h))
+        self.person = person  # имя персонажа
+        self.difference = None  # расстояние, на которое персонаж будет отставать отставать от игрока
+        self.w, self.h = 19, 40  # высота и ширина
+        self.image = pygame.Surface((self.w, self.h))
         self.image.fill(pygame.Color(COLOR))
-        self.rect = pygame.Rect(x, y, w, h)
+        self.rect = pygame.Rect(x, y, self.w, self.h)
         self.image.set_colorkey(pygame.Color(COLOR))
-        self.flag = False
+        self.flag = False  # бежит ли за игроком
         self.reasons = '0'
         self.delay, self.right, self.left, self.jump_right, self.jump_left, self.stay = get_animation(person)
+        # получение анимации персонажа
         self.stay.blit(self.image, (0, 0))  # По умолчанию, стоим
 
     def exit(self, h, w):  # найден выход персонажем
@@ -62,12 +64,13 @@ class Students(pygame.sprite.Sprite):
             return True
         return False
 
-    def move(self, coods_list, difference):
-        if difference != 0:
+    def move(self, coods_list, difference):  # двигаемся
+        if difference != 0:  # записываем последнее направление, куда двигался игрок
             self.difference = difference
-        if self.flag and len(coods_list) > 1:
+        if self.flag and len(coods_list) > 1:  # если игрок добежал до персонажа и в списке есть координаты
             self.rect.x, self.rect.y = coods_list[-1][0] - self.difference, coods_list[-1][1]
-            del coods_list[-1]
+            # передвигается
+            del coods_list[-1]  # удаляются последние координаты игрока
 
-    def get_position(self):
-        return self.rect.x, self.rect.y, self.rect.right - self.rect.left, self.rect.bottom - self.rect.top
+    def get_position(self):  # получение координат персонажа
+        return self.rect.x, self.rect.y, self.w, self.h
